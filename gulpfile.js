b47644af430bd78bcc8a73f5c8b5b900120ede70
Moko <moko75@hotmail.com>
2021-05-29 13:08:00 -0700

    'Updates'

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+"use strict";
+
+let gulp = require("gulp"),
+  autoprefixer = require("gulp-autoprefixer"),
+  exec = require("gulp-exec"),
+  browserSync = require("browser-sync").create(),
+  cp = require("child_process");
+
+
+
+gulp.task("css", function () {
+  return gulp
+    .src("_assets/css/**/*.css")
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./docs/css/"))
+    .pipe(browserSync.stream({ match: "**/*.css" }));
+});
+
+
+// Jekyll
+function jekyll() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" });
+  }
+
+
+
+gulp.task("jekyll", function () {
+
+   return gulp
+    .src(["./*.html", "./layout/**/*.html", "./_posts/**/*.markdown"])
+    .pipe(exec("jekyll build"))
+    .pipe(exec.reporter());
+});
+
+gulp.task("watch", function () {
+  browserSync.init({
+    server: {
+      baseDir: "./docs/",
+    },
+  });
+
+  gulp.watch("_assets/css/**/*.css", gulp.series("css"));
+
+  gulp.watch("docs/**/*.html").on("change", browserSync.reload);
+  gulp.watch("docs/**/*.js").on("change", browserSync.reload);
+
+  gulp.watch(
+	[
+	  "./*.html",
+	  "./_includes/*.html",
+	  "./_layouts/*.html",
+	  "./_posts/**/*"
+	]
+  ).on('change', gulp.series('jekyll', "css") 
+  );
+});
+
+gulp.task("default", gulp.series("css", "watch"));
