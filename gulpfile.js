b134ce4d00c4ed66020714266d15531addc9c771
Moko <moko75@hotmail.com>
2021-06-17 11:18:10 -0700

    Build Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,60 @@
 "use strict";
 
 let gulp = require("gulp"),
-	autoprefixer = require("gulp-autoprefixer"),
-	exec = require("gulp-exec"),
-	browserSync = require('browser-sync').create(),
-	sass = require('gulp-sass'),
-	cp = require("child_process");
+  autoprefixer = require("gulp-autoprefixer"),
+  exec = require("gulp-exec"),
+  browserSync = require("browser-sync").create(),
+  sass = require("gulp-sass"),
+  cp = require("child_process");
 
-gulp.task("sass", function() {
-	return gulp.src( '_assets/scss/*.scss' )
-	
-		.pipe( sass().on('error', sass.logError) )
-		.pipe( autoprefixer() )
-		
-		.pipe( gulp.dest( './docs/css/' ) )
-		.pipe( browserSync.stream({ match: '**/*.css' }) )
-	;
+gulp.task("sass", function () {
+  return gulp
+    .src("_assets/scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./docs/css/"))
+    .pipe(browserSync.stream({ match: "**/*.css" }));
 });
 
 // Jekyll
-gulp.task("jekyll-dev", function() {
-	return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl ''"], { stdio: "inherit", shell: true });
+gulp.task("jekyll-dev", function () {
+  return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl ''"], {
+    stdio: "inherit",
+    shell: true,
+  });
 });
 
 // Jekyll
-gulp.task("jekyll", function() {
-	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true });
+gulp.task("jekyll", function () {
+  return cp.spawn("bundle", ["exec", "jekyll", "build"], {
+    stdio: "inherit",
+    shell: true,
+  });
 });
 
-gulp.task("watch", function() {
+gulp.task("watch", function () {
+  browserSync.init({
+    server: {
+      baseDir: "./docs/",
+    },
+  });
 
-	browserSync.init({
-		server: {
-            baseDir: "./docs/"
-		}
-	});
+  gulp.watch("_assets/scss/*.scss", gulp.series("sass"));
 
-	gulp.watch( '_assets/scss/*.scss', gulp.series('sass') );
+  gulp
+    .watch([
+      "./*.html",
+      "./*.yml",
+      "./_includes/*.html",
+      "./_layouts/*.html",
+      "./_posts/**/*.*",
+    ])
+    .on("change", gulp.series("jekyll", "sass"));
 
-	gulp.watch(
-		[
-			"./*.html",
-			"./*.yml",
-			"./_includes/*.html",
-			"./_layouts/*.html",
-			"./_posts/**/*.*"
-		]
-	).on('change', gulp.series('jekyll', 'sass') );
-
-	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
-	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
+  gulp.watch("docs/**/*.html").on("change", browserSync.reload);
+  gulp.watch("docs/**/*.js").on("change", browserSync.reload);
 });
 
 
-gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
-	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
-}));
 
-gulp.task("default", gulp.series('jekyll', 'sass', 'watch'));
-
+gulp.task("default", gulp.series("jekyll", "sass", "watch"));
